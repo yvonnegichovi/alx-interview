61a96f49ad716b5ea69fde187840091deb8971ce
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-09 16:47:47 +0300

    task 0
    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,3 +10,16 @@ def canUnlockAll(boxes):
     """
     if not boxes:
         return False
+
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    stack = [0]
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True
+                stack.append(key)
+
+    return all(opened)
