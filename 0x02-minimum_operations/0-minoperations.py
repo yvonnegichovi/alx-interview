ffb62ab68218be5abdb10dbaf43357158b0ce44a
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-16 18:49:44 +0300

    Task 0.1
    first step. If n is impossible to achieve, return 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,3 +2,16 @@
 """
 This module works on minimum operations
 """
+
+
+def minOperations(n):
+    """
+    This method calculates the fewest number of operations needed to result in
+    exactly n H characters in the file
+    The text editor can execute only two operations in this file:
+        - Copy All
+        - Paste
+    Returns an integer
+    """
+    if not n:
+        return 0
