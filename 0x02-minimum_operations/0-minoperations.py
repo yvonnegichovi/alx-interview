67820a9537422bfd3552f604d12eb8a1975250c5
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-16 21:04:54 +0300

    Task 0.2
    Working out the prime factors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,3 +15,13 @@ def minOperations(n):
     """
     if not n:
         return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
