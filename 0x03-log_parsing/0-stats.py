8fc74fc6c88edb989e96c5dd33fb3810903656d7
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-23 14:04:46 +0300

    handles signal

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,3 +2,16 @@
 """
 This script reads stdin line by line and computes metrics
 """
+
+import signal
+import sys
+
+
+def handle_signal():
+    """
+    Handles signal
+    """
+    signal.sigint
+
+with open('filename', 'r', encoding="utf-8") as f:
+    read_data = f.read()
