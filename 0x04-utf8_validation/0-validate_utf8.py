2244704d02c49755e7950a4747c4e0366323a4a9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-25 00:16:27 +0300

    alx interview on UTF-8 encoding task begins

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is about UTF-8 encoding task
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
+    """
