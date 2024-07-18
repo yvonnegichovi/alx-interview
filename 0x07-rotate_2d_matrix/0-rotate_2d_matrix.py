df58530ae851bd5498138c0d9b5bf2755ea1576c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-19 01:07:22 +0300

    2D matrix task for interview preparation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,7 @@ def rotate_2d_matrix(matrix):
     returns:
         - None
     """
-    n = len(maitrix)
+    n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
