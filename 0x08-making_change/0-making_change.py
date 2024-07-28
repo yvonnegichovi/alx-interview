fe685b9b4e77d67c7038e87df02878209f194387
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-28 13:48:55 +0300

    a test 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,17 @@
 
 def makeChange(coins, total):
     """ Determines the fewest number of coins needed to a given amount total
+    Args:
+        - coins: a list of the values of coins in your possession
+        - total: a given amount
     Return: fewest number of coins needed to meet total"""
-    if total is 0:
+    if total <= 0:
         return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if amount - coin >= 0:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
+
