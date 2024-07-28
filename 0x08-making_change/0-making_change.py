cb08437b2cc7df9299832b2430aa15f1df60560f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-28 13:12:04 +0300

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,4 +3,7 @@
 
 
 def makeChange(coins, total):
-    """ Determines the fewest number of coins needed to a given amount total"""
+    """ Determines the fewest number of coins needed to a given amount total
+    Return: fewest number of coins needed to meet total"""
+    if total is 0:
+        return 0
