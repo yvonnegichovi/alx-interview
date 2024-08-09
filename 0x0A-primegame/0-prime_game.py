5af90ea59682208a99b2aa2d1d97f2d6bba55f87
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-09 14:38:42 +0300

    execute

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,6 @@ def isWinner(x, nums):
     Parameters:
     - x: Number of rounds
     - nums: List of integers where each represents n in a round
-    
     Returns:
     - Name of the player with the most wins or None if it's a tie
     """
@@ -17,8 +16,7 @@ def isWinner(x, nums):
 
     max_num = max(nums)
     primes = [True] * (max_num + 1)
-    primes[0] = primes[1] = False 
-
+    primes[0] = primes[1] = False
     for i in range(2, int(max_num**0.5) + 1):
         if primes[i]:
             for j in range(i*i, max_num + 1, i):
