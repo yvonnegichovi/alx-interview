7e1689551d2d9ff5e809fcaaeeb13aa560435a9c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-09 14:26:49 +0300

    execute

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
+
 def isWinner(x, nums):
     """
     Determines the winner of the prime game.
-    
     Parameters:
     - x: Number of rounds
     - nums: List of integers where each represents n in a round
@@ -13,22 +14,19 @@ def isWinner(x, nums):
     if x <= 0 or not nums:
         return None
 
-    # Precompute prime numbers up to the maximum value in nums
     max_num = max(nums)
     primes = [True] * (max_num + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not primes
+    primes[0] = primes[1] = False 
 
     for i in range(2, int(max_num**0.5) + 1):
         if primes[i]:
             for j in range(i*i, max_num + 1, i):
                 primes[j] = False
 
-    # Count the number of primes up to each number in nums
     prime_count = [0] * (max_num + 1)
     for i in range(1, max_num + 1):
         prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
 
-    # Determine the winner for each round
     maria_wins = 0
     ben_wins = 0
 
